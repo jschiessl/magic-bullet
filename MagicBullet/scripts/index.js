2fcb548abb78e0f9b3819615498f2366ec949511
Jonas Schießl <jonas.schiessl@tekaris.com>
2014-05-30 18:49:30 +0200

    Blank Cordova App with Typescript

---
+++ MagicBullet/scripts/index.js
@@ -0,0 +1,36 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397705
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints,
+// and then run "window.location.reload()" in the JavaScript Console.
+var MagicBullet;
+(function (MagicBullet) {
+    "use strict";
+
+    (function (Application) {
+        function initialize() {
+            document.addEventListener('deviceready', onDeviceReady, false);
+        }
+        Application.initialize = initialize;
+
+        function onDeviceReady() {
+            // Handle the Cordova pause and resume events
+            document.addEventListener('pause', onPause, false);
+            document.addEventListener('resume', onResume, false);
+            // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        }
+
+        function onPause() {
+            // TODO: This application has been suspended. Save application state here.
+        }
+
+        function onResume() {
+            // TODO: This application has been reactivated. Restore application state here.
+        }
+    })(MagicBullet.Application || (MagicBullet.Application = {}));
+    var Application = MagicBullet.Application;
+
+    window.onload = function () {
+        Application.initialize();
+    };
+})(MagicBullet || (MagicBullet = {}));
+//# sourceMappingURL=index.js.map
